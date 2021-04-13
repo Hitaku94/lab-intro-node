f5de4d64118022591e9a715bcb9b0d5b8c828e90
Takayuki Wada <takayuki.wada@hotmail.fr>
2021-04-13 17:55:53 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort((a, b) => a - b);
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] < this.length) {
+      return this.items[pos];
+    }
 
-  max() {}
+    throw new Error("OutOfBounds");
+  }
 
-  min() {}
+  max() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+    return this.items[this.length - 1];
+  }
 
-  avg() {}
+  min() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[0];
+  }
+
+  sum() {
+    return this.items.reduce((acc, elem) => {
+      return acc + elem;
+    }, 0);
+  }
+
+  avg() {
+    if (this.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
